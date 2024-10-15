1392d3adc06dcaaeaaab2e470bb87a1084f2bb4b
AkkarawatKavee <147287220+AkkarawatKavee@users.noreply.github.com>
2024-10-16 00:08:37 +1100

    Delete sprint3 directory

--- sprint3/src/sprint3code/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/ros2_ws/src/sprint/sprint3/src/sprint3code
-# Build directory: /home/student/ros2_ws/src/sprint/sprint3/src/sprint3code/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
